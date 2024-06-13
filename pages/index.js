9f0b8f1f474dac388e9d9c092b60692912e6a6fe
Jose Gutierro <jose.gutierro@estudiocactus.com>
2024-06-13 11:14:38 +0200

    Update project

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home () {
-  return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-      ESTUDIO CACTUS VISUALIZER TEST
-    </div>
-  )
-}
